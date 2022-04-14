77eae571fbd1aacd74eb3300db1cd91b39161134
Tanya <thetwtanya@gmail.com>
2022-04-14 23:53:15 +0800

    add image count

--- r2.py
+++ r2.py
@@ -12,8 +12,10 @@ def convert(lines):
     person = None # person這個值先宣告"無"
     allen_word_count = 0
     allen_sticker_count = 0
+    allen_image_count = 0
     viki_word_count = 0
     viki_sticker_count = 0
+    viki_image_count = 0
     for line in lines:  # 一行一行讀取對話紀錄
         s = line.split(' ')
         time = s[0]
@@ -21,17 +23,25 @@ def convert(lines):
         if name == 'Allen':
             if s[2] == '貼圖':
                 allen_sticker_count += 1
+            elif s[2] == '圖片':
+                allen_image_count += 1
             else:
                 for m in s[2:]:
                     allen_word_count += len(m)
         elif name == 'Viki':
             if s[2] == '貼圖':
                 viki_sticker_count += 1
+            elif s[2] == '圖片':
+                viki_image_count += 1
             else:
                 for m in s[2:]:
                     viki_word_count += len(m)        
-    print('allen說了', allen_word_count, '傳了', allen_sticker_count, '個貼圖')
-    print('viki說了', viki_word_count, '傳了', viki_sticker_count, '個貼圖')   
+    print('allen說了', allen_word_count)
+    print('allen傳了', allen_sticker_count, '個貼圖')
+    print('allen傳了', allen_image_count, '張圖片')
+    print('viki說了', viki_word_count)
+    print('viki傳了', viki_sticker_count, '個貼圖')
+    print('viki傳了', viki_image_count, '張圖片')   
         # print(s)
 
 def write_file(filename, lines):
